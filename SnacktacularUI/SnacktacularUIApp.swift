91590e519b23ec154c88339e6f496f1f28cd4885
Bob Witmer <bwitmer@golden.net>
2023-08-21 16:24:39 -0400

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Bob Witmer on 2023-08-21.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
