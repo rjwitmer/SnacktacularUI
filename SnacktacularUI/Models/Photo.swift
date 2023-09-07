1adefbab0959156007a1c94dde37f16fffd9bf28
Bob Witmer <bwitmer@golden.net>
2023-09-07 15:17:42 -0400

    Chapter 8.18 Part I - Completed

---
+++ SnacktacularUI/Models/Photo.swift
@@ -0,0 +1,23 @@
+//
+//  Photo.swift
+//  SnacktacularUI
+//
+//  Created by Bob Witmer on 2023-09-07.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Photo: Identifiable, Codable {
+    @DocumentID var id: String?
+    var imageURLString = "" // This will hold the URL for loading the image
+    var description = ""
+    var reviewer = Auth.auth().currentUser?.email ?? ""
+    var postedOn = Date()
+    
+    var dictionary: [String: Any] {
+        return ["imageURLString": imageURLString, "description": description, "reviewer": reviewer, "postedOn": Timestamp(date: Date())]
+    }
+    
+}
--- SnacktacularUI/ViewModels/SpotViewModel.swift
+++ SnacktacularUI/ViewModels/SpotViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseFirestore
+import UIKit
+import FirebaseStorage
 
 @MainActor
 
@@ -37,6 +39,58 @@ class SpotViewModel: ObservableObject {
                 print("😡 ERROR: Could not create a new spot in '\(collectionName)' --> \(error.localizedDescription)")
                 return false
             }
+        }
+    }
+    
+    func saveImage(spot: Spot, photo: Photo, image: UIImage) async -> Bool {
+        guard let spotID = spot.id else {
+            print("😡 ERROR: spot.id == nil")
+            return false
+        }
+        
+        let photoName = UUID().uuidString   // This will be the name of the image file
+        let storage = Storage.storage()     // Create a Firebase Cloud Storage instance
+        let storageRef = storage.reference().child("\(spotID)/\(photoName).jpeg")
+        
+        guard let resizedImage = image.jpegData(compressionQuality: 0.2) else {
+            print("😡 ERROR: Could not resize/compress image")
+            return false
+        }
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"  // Setting metadata allows you to see console image in the web browser. This setting works for both png and jpg.
+        
+        var imageURLString = ""     // We'll set this after the image is successfully saved
+        
+        do {
+            let _ = try await storageRef.putDataAsync(resizedImage, metadata: metadata)
+            print("📸 Image Saved!")
+            do {
+                let imageURL = try await storageRef.downloadURL()
+                imageURLString = "\(imageURL)"  // We'll save this to Cloud Firestore as part of document in 'photos' collection, below
+            } catch {
+                print("😡 ERROR: Capturing downloadURL() in imageURL --> \(error.localizedDescription)")
+                return false
+            }
+
+        } catch {
+            print("😡 ERROR: uploading image to Firebase Cloud Storage --> \(error.localizedDescription)")
+            return false
+        }
+        
+        // Now save to the 'photos' collection of the spot document 'spotID'
+        let db = Firestore.firestore()
+        let collectionString = "spots\(spotID)/photos"
+        
+        do {
+            var newPhoto = photo
+            newPhoto.imageURLString = imageURLString
+            try await db.collection(collectionString).document(photoName).setData(newPhoto.dictionary)
+            print("😎 Data updated successfully")
+            return true
+        } catch {
+            print("😡 ERROR: Could not update data in '\(collectionString).document(\(photoName))' --> \(error.localizedDescription)")
+            return false
         }
     }
     
